34c17347ebb25d714e96a84d848e6c9fdb0cd6b5
PimGroeneveld <pimgroeneveld92@gmail.com>
2019-02-14 13:29:32 +0100

    player end point set up

--- src/main/java/com/capgemini/chesstournament/api/ChessPlayerEndPoint.java
+++ src/main/java/com/capgemini/chesstournament/api/ChessPlayerEndPoint.java
@@ -1,4 +1,44 @@
 package com.capgemini.chesstournament.api;
 
+import com.capgemini.chesstournament.controller.service.ChessPlayerService;
+import com.capgemini.chesstournament.model.ChessPlayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tournament")
 public class ChessPlayerEndPoint {
+
+    @Autowired
+    private ChessPlayerService chessPlayerService;
+
+    @GetMapping("/allplayers")
+    public Iterable<ChessPlayer> getAllChessPlayers() {
+        Iterable<ChessPlayer> chessPlayers = chessPlayerService.showAllChessPlayers();
+        return chessPlayers;
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @PostMapping("/allplayers/add")
+    public void addChessPlayer(@RequestBody ChessPlayer chessPlayer) {
+        chessPlayerService.addChessPlayer(chessPlayer);
+    }
+
+    @GetMapping("/allplayers/{id}")
+    public ChessPlayer getChessPlayerById(@PathVariable Long id) {
+        Optional<ChessPlayer> chessPlayer = chessPlayerService.showChessPlayerById(id);
+        return chessPlayer.get();
+    }
+
+    @DeleteMapping("/allplayers/{id}/delete")
+    public void deleteGuest(@PathVariable Long id) {
+        chessPlayerService.removeChessPlayerById(id);
+        System.out.println("Deleted chess player endPoint id: " + id);
+    }
+
+
 }
